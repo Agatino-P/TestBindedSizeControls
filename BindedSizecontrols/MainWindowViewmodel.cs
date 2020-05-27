e7831ebc0ee890ee3c28e47b635546bc551ba6d5
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-27 23:30:34 +0200

    Add project files.

---
+++ BindedSizecontrols/MainWindowViewmodel.cs
@@ -0,0 +1,31 @@
+﻿using BindedSizecControls;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace BindedSizeControls
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private ObservableCollection<SizeableItem> _items= new ObservableCollection<SizeableItem>();
+        public ObservableCollection<SizeableItem> Items { get => _items; set { Set(() => Items, ref _items, value); }}
+
+        private bool _isTwoFields;
+        public bool IsTwoFields { get => _isTwoFields; set { Set(() => IsTwoFields, ref _isTwoFields, value); }}
+
+        public MainWindowViewModel()
+        {
+            loadSampleData();
+        }
+
+        private void loadSampleData()
+        {
+            Items.Add(new SizeableItem(1, 1));
+            Items.Add(new SizeableItem(3 , 2));
+
+        }
+    }
+}
---
+++ BindedSizecontrols/SizeableItem.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace BindedSizeControls
+{
+    public class SizeableItem : ViewModelBase
+    {
+        private string _name; public string Name { get => $"{Column}-{Size}"; set { Set(() => Name, ref _name, value); }}
+
+        
+        private int _column; 
+        public int Column { get => _column; set { Set(() => Column, ref _column, value); RaisePropertyChanged(nameof(Name)); } }
+
+        private int _size;
+        public int Size { get => _size; set { Set(() => Size, ref _size, value) ; RaisePropertyChanged(nameof(Name)); } }
+
+
+        public SizeableItem() : this(1,1)
+        {
+            
+        }
+
+        public SizeableItem(int column=1, int size=1)
+        {
+
+            Column = column;
+            Size = size;
+        }
+    }
+}
